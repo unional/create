486af35111fd79a586b5df57549bf3c9a87ab1c3
Homa Wong (unional) <homawong@gmail.com>
2019-08-18 02:00:06 -0700

    feat: rename uni-cli to create

--- jest.config.js
+++ jest.config.js
@@ -3,11 +3,11 @@ const base = require('@unional/devpkg-node/simple/config/jest.common')
 module.exports = {
   ...base,
   projects: [
-    '<rootDir>/packages/*/src'
+    '<rootDir>/packages/*/src',
   ],
   testMatch: [
     '**/*.accept.ts',
     '**/*.spec.ts',
-    '**/*.node-spec.ts'
-  ]
+    '**/*.node-spec.ts',
+  ],
 }
