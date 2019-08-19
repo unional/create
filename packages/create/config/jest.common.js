1bdd0a917f02d5f4216386b5b1f7e61ed6822f86
Homa Wong (unional) <homawong@gmail.com>
2019-08-18 18:26:24 -0700

    docs: update readme and clean up deps

---
+++ packages/create/config/jest.common.js
@@ -0,0 +1,60 @@
+const isCI = require('is-ci')
+module.exports = isCI ? {
+  'preset': 'ts-jest',
+  'globals': {
+    'ts-jest': {
+      'diagnostics': false,
+    },
+  },
+  'collectCoverageFrom': [
+    '<rootDir>/src/**/*.[jt]s',
+    '!<rootDir>/src/bin.[jt]s',
+  ],
+  'roots': [
+    '<rootDir>/src',
+  ],
+  'reporters': [
+    'default',
+    [
+      'jest-junit',
+      {
+        'output': '.reports/junit/js-test-results.xml',
+      },
+    ],
+  ],
+  'testEnvironment': 'node',
+  'testMatch': ['**/?(*.)+(spec|test|integrate|accept|system|unit).[jt]s?(x)'],
+} : {
+    'preset': 'ts-jest',
+    'globals': {
+      'ts-jest': {
+        'diagnostics': false,
+      },
+    },
+    'collectCoverageFrom': [
+      '<rootDir>/src/**/*.[jt]s',
+      '!<rootDir>/src/bin.[jt]s',
+    ],
+    'reporters': [
+      'default',
+      'jest-progress-tracker',
+      ['jest-audio-reporter', { volume: 0.3 }],
+    ],
+    'roots': [
+      '<rootDir>/src',
+    ],
+    'testEnvironment': 'node',
+    'testMatch': ['**/?(*.)+(spec|test|integrate|accept|system|unit).[jt]s?(x)'],
+    'watchPlugins': [
+      'jest-watch-suspend',
+      'jest-watch-repeat',
+      'jest-watch-typeahead/filename',
+      'jest-watch-typeahead/testname',
+      [
+        'jest-watch-toggle-config', { 'setting': 'verbose' },
+      ],
+      [
+        'jest-watch-toggle-config', { 'setting': 'collectCoverage' },
+      ],
+    ],
+  }
